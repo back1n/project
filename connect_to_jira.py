a615a0216eaf44787424568dc01b22d7310c8bb6
Ilya Martin <martinqwq4@gmail.com>
2026-05-24 18:31:50 +0300

    Подключение и авторизация на atlassian.net

--- connect_to_jira.py
+++ connect_to_jira.py
@@ -1,2 +1,9 @@
 from read_to_config import *
 import requests
+from requests.auth import HTTPBasicAuth
+
+
+response = requests.get(
+    url=JIRA_URL,
+    auth=HTTPBasicAuth(USERNAME, PASSWORD_OR_TOKEN)
+)
