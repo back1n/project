e1c877d495969984cbc0d015b93274b71b825c77
Ilya Martin <martinqwq4@gmail.com>
2026-05-24 17:12:13 +0300

    Создание нового файла для подключения к jira и смена имени файла

--- connect_to_jira.py
+++ connect_to_jira.py
@@ -1,34 +1,4 @@
-import requests
-import json
-import logging
+from read_to_config import *
 
 
-try:
-    with open('config.json', 'r', encoding='utf-8') as file:
-        data = json.load(file)
-        try:
-            LOG_FILE = data['log_file']
-        except KeyError:
-            LOG_FILE = "jira_rating_check.log"
-        finally:
-            logging.basicConfig(
-                filename=LOG_FILE,  # Имя файла для логов
-                level=logging.DEBUG,
-                format='%(asctime)s - %(levelname)s - %(message)s',
-                encoding='utf-8'
-            )
-
-        JIRA_URL = data['jira_url']
-        USERNAME = data['username']
-        PASSWORD_OR_TOKEN = data['password']
-
-        logging.info('Файл успешно прочитан')
-        
-except FileNotFoundError:
-    raise("не обнаружен файл config.json")
-
-except KeyError as key:
-    logging.error(f"Не обнаржуен параметр {key}")
-
-
-
+print(JIRA_URL)
---
+++ read_to_config.py
@@ -0,0 +1,33 @@
+import json
+import logging
+
+
+try:
+    with open('config.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        try:
+            LOG_FILE = data['log_file']
+        except KeyError:
+            LOG_FILE = "jira_rating_check.log"
+        finally:
+            logging.basicConfig(
+                filename=LOG_FILE,  # Имя файла для логов
+                level=logging.DEBUG,
+                format='%(asctime)s - %(levelname)s - %(message)s',
+                encoding='utf-8'
+            )
+
+        JIRA_URL = data['jira_url']
+        USERNAME = data['username']
+        PASSWORD_OR_TOKEN = data['password']
+
+        logging.info('Файл успешно прочитан')
+        
+except FileNotFoundError:
+    raise("не обнаружен файл config.json")
+
+except KeyError as key:
+    logging.error(f"Не обнаржуен параметр {key}")
+
+
+
