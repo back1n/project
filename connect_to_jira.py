d1336c6332194216b42742e42b5b963470de27cc
Ilya Martin <martinqwq4@gmail.com>
2026-05-24 13:44:03 +0300

    First commit

---
+++ connect_to_jira.py
@@ -0,0 +1,29 @@
+import requests
+import json
+import logging
+
+
+try:
+    with open('config.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+        LOG_FILE = data['log_file']
+        logging.basicConfig(
+            filename=LOG_FILE,  # Имя файла для логов
+            level=logging.DEBUG,
+            format='%(asctime)s - %(levelname)s - %(message)s',
+            encoding='utf-8'
+        )
+
+        JIRA_URL = data['jira_url']
+        USERNAME = data['username']
+        PASSWORD_OR_TOKEN = data['password']
+
+        logging.info('Файл усмешно прочитан')
+        
+except FileNotFoundError:
+    raise("не обнаружен файл config.json")
+
+except KeyError as key:
+    logging.error(f"Не обнаржуен ключ {key}")
+
