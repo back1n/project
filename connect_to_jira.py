93da9d60fe82ed40c27279f8f54ff69f696b5940
Ilya Martin <martinqwq4@gmail.com>
2026-05-24 17:08:05 +0300

    Корректировка чтения файла config.json

--- connect_to_jira.py
+++ connect_to_jira.py
@@ -6,24 +6,29 @@ import logging
 try:
     with open('config.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
-
-        LOG_FILE = data['log_file']
-        logging.basicConfig(
-            filename=LOG_FILE,  # Имя файла для логов
-            level=logging.DEBUG,
-            format='%(asctime)s - %(levelname)s - %(message)s',
-            encoding='utf-8'
-        )
+        try:
+            LOG_FILE = data['log_file']
+        except KeyError:
+            LOG_FILE = "jira_rating_check.log"
+        finally:
+            logging.basicConfig(
+                filename=LOG_FILE,  # Имя файла для логов
+                level=logging.DEBUG,
+                format='%(asctime)s - %(levelname)s - %(message)s',
+                encoding='utf-8'
+            )
 
         JIRA_URL = data['jira_url']
         USERNAME = data['username']
         PASSWORD_OR_TOKEN = data['password']
 
-        logging.info('Файл усмешно прочитан')
+        logging.info('Файл успешно прочитан')
         
 except FileNotFoundError:
     raise("не обнаружен файл config.json")
 
 except KeyError as key:
     logging.error(f"Не обнаржуен параметр {key}")
+
+
 
